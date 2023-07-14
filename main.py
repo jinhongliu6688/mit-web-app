f9763f676a74b51f498f55bc0d87f6a3157859ed
jinhongliu6688 <jinhongliu6688@gmail.com>
2023-07-13 23:34:11 -0700

    update

--- main.py
+++ main.py
@@ -159,4 +159,4 @@ def start(request: Request):
             })
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=9999, debug=True)
+    uvicorn.run(app, host='0.0.0.0', port=9999, debug=True, workers=2)
